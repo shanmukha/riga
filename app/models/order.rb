f0389e674cbfb157ccb351add490cdb967fd8bdc
shanmukha <shanmukhakatta@gmail.com>
2010-02-11 15:23:48 +0500

    order model changes

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,12 +6,13 @@ class Order < ActiveRecord::Base
 
   def purchase
     response = EXPRESS_GATEWAY.purchase(10, :ip => buyer.ip_address, :token => self.express_token, :payer_id => self.express_payer_id)
-    transactions.create!(:action => "purchase", :amount => 10, :response => response)
+    #transactions.create!(:action => "purchase", :amount => 10, :response => response)
+    OrderTransaction.create!(:action => "purchase",:order_id => self.id, :amount => 10, :response => response)
     response.success?
   end
-  
+
   def price_in_cents
     (cart.total_price*100).round
-  end  
- 
+  end
+
 end
