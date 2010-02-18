40a5075027cfd59f04b83dfc47dbbc821370fc1a
shanmukha <shanmukhakatta@gmail.com>
2010-02-18 15:19:10 +0500

    admin order changes

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,6 +5,6 @@ class Admin::OrdersController < ApplicationController
   def index
     @search = Order.ascend_by_created_at.search(params[:search])
     @search.created_at_like = params[:search][:created_at_like].to_date if !params[:search].nil?
-    @orders = @search.all(:include => [:buyer, :transaction]).paginate :page => params[:page],:per_page =>1
+    @orders = @search.all(:include => [:buyer, :transaction]).paginate :page => params[:page],:per_page =>50
   end
 end
