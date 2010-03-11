30f12a9cb94fe49409ba5cfc8ade8783812a2c4b
isiri <isiri@Vijendra-desktop.(none)>
2010-03-11 15:29:18 +0530

    paypal button

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,18 @@ class OrdersController < ApplicationController
   def new
     @order = Order.new
     @buyer = @order.build_buyer
+    #We got the transaction id. Now checking the status of the transaction by posting it to PDT.
+    resp = pdt_post(params['tx'])
+    if resp.body =~ /SUCCESS/
+      result = split_response(resp.body)
+      @order.payer_id = result['payer_id']
+      @buyer.name = result['name']
+      @buyer.email = result['email']
+      flash.now[:notice] = "Thank you. Your paypal transaction id is #{params['tx']}. Please confirm that, your personal details provided below are correct."
+    else
+      flash[:error] = "Your transaction id is #{params['tx']}. There was an error, while getting the transaction status. Please contact administrator at email riga_admin@gmail.com and provide your transaction id."
+      redirect_to root_url
+    end
   end
 
   def create
@@ -14,7 +26,8 @@ class OrdersController < ApplicationController
       render :action => "new" 
     else
       if @order.save
-        redirect_to(express_checkout_new_order_url(:id => @order.id))  
+        res = Mechanize.new.post("https://www.sandbox.paypal.com/cgi-bin/webscr", 'cmd' => '_s-xclick', 'hosted_button_id' => '7WFL7WVZAW9RE')
+        #redirect_to(express_checkout_new_order_url(:id => @order.id))  
       else
         @buyer = @order.build_buyer
         render :action => "new"  
@@ -44,5 +57,18 @@ class OrdersController < ApplicationController
     order = Order.find(params[:id])
     order.buyer.update_attribute('guide_token', order.buyer.make_token)
     GuideSender.deliver_send_guide(order)
+  end
+
+ private
+  
+  def pdt_post(tx_value)
+    Mechanize.new.post("https://www.sandbox.paypal.com/cgi-bin/webscr", 'cmd' => '_notify-synch', 'tx'=> tx_value, 'at' => 'ujtZeP0hhIK9mUKgCC_oc5N0CICy8GpHD9tapxXcXebJbTbELKJWE1RgVLW') 
+  end
+
+  def split_response(str_resp)
+   result = {}
+   result['payer_id'] = str_resp.split('payer_id=')[1].split(' ').first
+   result['name'] = 'Vijendra'
+   result['email'] = 'e@email.com'
   end
 end
